ccba2ad19e014e85acf01f767140724250145fee
Oktay Rasimov <oktayrasimov123@gmail.com>
2023-09-19 04:04:11 +0300

    start searching fix

--- src/ui/StartSearching.jsx
+++ src/ui/StartSearching.jsx
@@ -3,6 +3,8 @@ import styled from "styled-components";
 const MainTitle = styled.h1`
   color: var(--color-light-100);
   padding-left: 2rem;
+  text-align: center;
+  padding-top: 10%;
 `;
 
 function StartSearching() {
